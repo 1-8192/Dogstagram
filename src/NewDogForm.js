b358825b122a6a98492753bdbff3de12eb83d750
1-8192 <allegranzia@gmail.com>
2019-04-30 23:09:50 -0400

    worked on styling more

--- src/NewDogForm.js
+++ src/NewDogForm.js
@@ -36,12 +36,29 @@ class NewDogForm extends React.Component {
       return null
     } else {
       return (
-        <form className="card" onSubmit={this.handleNewSubmitLocally}>
-          <input onChange={this.handleFormChange} type="text" name="name" value={this.state.name} placeholder="Dawg Name"/>
-          <input onChange={this.handleFormChange} type="text" name="breed" value={this.state.breed} placeholder="Breed"/>
-          <input onChange={this.handleFormChange} type="text" name="imgUrl" value={this.state.imgUrl} placeholder="gif url"/>
-          <input className="waves-effect waves-light btn" type="Submit" value="submit"/>
+        <div className="row">
+        <form className="col s-12" onSubmit={this.handleNewSubmitLocally}>
+          <div className="row">
+            <div className="input field col s-12">
+              <input onChange={this.handleFormChange} type="text" name="name" value={this.state.name} placeholder="Dawg Name"/>
+              <label>NAME</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input field col s-12">
+              <input onChange={this.handleFormChange} type="text" name="breed" value={this.state.breed} placeholder="Breed"/>
+              <label>BREED</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input field col s-12">
+              <input onChange={this.handleFormChange} type="text" name="imgUrl" value={this.state.imgUrl} placeholder="gif url"/>
+              <label>IMAGE URL</label>
+            </div>
+          </div>
+          <input className="waves-effect green lighten-4 btn" type="Submit" value="submit"/>
         </form>
+      </div>
       )
     }
   }
@@ -49,7 +66,7 @@ class NewDogForm extends React.Component {
   render() {
     return (
       <div>
-        <button className="waves-effect waves-light btn" onClick={this.revealClick}>Add a dawg!</button>
+        <button className="waves-effect green lighten-4 btn" onClick={this.revealClick}>Add a dawg!</button>
         {this.form()}
       </div>
     )
